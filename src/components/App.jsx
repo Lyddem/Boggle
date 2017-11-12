3f087e4f8daaec66faab7075f981da2df335f193
Lyddem <lydiademi@gmail.com>
2017-11-11 17:26:45 -0800

    Rendered board

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,55 +8,82 @@ class App extends React.Component {
 
     this.state = {
       data: [],
-      board: [],
+      board1: [],
+      board2: [],
+      board3: [],
+      board4: [],
+      board5: [],
       total: 0,
       currentWord: '',
-      dice: [
-        "aaafrs",
-        "aaeeee",
-        "aafirs",
-        "adennn",
-        "aeeeem"],
-        // "aeegmu",
-        // "aegmnn",
-        // "afirsy",
-        // "bjkqxz",
-        // "ccenst"
-        // "ceiilt",
-        // "ceilpt",
-        // "ceipst",
-        // "ddhnot",
-        // "dhhlor",
-        // "dhlnor",
-        // "dhlnor",
-        // "eiiitt",
-        // "emottt",
-        // "ensssu",
-        // "fiprsy",
-        // "gorrvw",
-        // "iprrry",
-        // "nootuw",
-        // "ooottu"]
+      row1: ["aaafrs","aaeeee","aafirs","adennn","aeeeem"],
+      row2: ["aeegmu","aegmnn","afirsy","bjkqxz","ccenst"],
+      row3: ["ceiilt","ceilpt", "ceipst","ddhnot","dhhlor"],
+      row4: ["dhlnor","dhlnor", "eiiitt","emottt","ensssu"],
+      row5: ["fiprsy","gorrvw", "iprrry","nootuw","ooottu"],
     }
 
     this.clicked = this.clicked.bind(this);
   }
 
 componentWillMount() {
-    var letter = '';
+    let letter1 = '';
+    let letter2 = '';
+    let letter3 = '';
+    let letter4 = '';
+    let letter5 = '';
 
-    this.state.dice.forEach(die => {
-      letter = die[Math.floor(Math.random() * 6)].toUpperCase();
-      if (letter === 'Q') {
-        this.state.board.push("Qu")
+    this.state.row1.forEach(die => {
+      letter1 = die[Math.floor(Math.random() * 6)].toUpperCase();
+      if (letter1 === 'Q') {
+        this.state.board1.push("Qu")
       } else {
-        this.state.board.push(letter);
+        this.state.board1.push(letter1);
+      }
+    })
+    this.state.row2.forEach(die => {
+      letter2 = die[Math.floor(Math.random() * 6)].toUpperCase();
+      if (letter2 === 'Q') {
+        this.state.board2.push("Qu")
+      } else {
+        this.state.board2.push(letter2);
+      }
+    })
+    this.state.row3.forEach(die => {
+      letter3 = die[Math.floor(Math.random() * 6)].toUpperCase();
+      if (letter3 === 'Q') {
+        this.state.board3.push("Qu")
+      } else {
+        this.state.board3.push(letter3);
+      }
+    })
+    this.state.row4.forEach(die => {
+      letter4 = die[Math.floor(Math.random() * 6)].toUpperCase();
+      if (letter4 === 'Q') {
+        this.state.board5.push("Qu")
+      } else {
+        this.state.board4.push(letter4);
+      }
+    })
+    this.state.row5.forEach(die => {
+      letter5 = die[Math.floor(Math.random() * 6)].toUpperCase();
+      if (letter5 === 'Q') {
+        this.state.board5.push("Qu")
+      } else {
+        this.state.board5.push(letter5);
       }
     })
   }
 
 clicked() {
-  alert('clicked');
+  //change background color
+  let letter = document.querySelector('.btn span').innerHTML;
+  console.log('LETTER', letter)
+  //add letter to currentWord
+  this.setState({
+    currentWord: this.state.currentWord + letter
+  })
+
+  console.log('letter', letter)
 }
 
 render () {
@@ -64,16 +91,54 @@ render () {
     <div id="container">
       <img id="title" src="http://qumani.com/qdserve/boggle-logo.png"/>
 
-
+    {/* Board */}
       <div id="board">
-          {this.state.board.map((letter, index) => {
+        <div class="row">
+          {this.state.board1.map((letter, index) => {
             return (
-              <div className="btn" onClick={this.clicked}>
-                <span> {letter}</span>
+              <div className="btn" value={letter} onClick={() => this.clicked({letter})}>
+                <span> {letter} </span>
               </div>
             )
           })}
-        </div><br />
+        </div>
+        <div class="row">
+        {this.state.board5.map((letter, index) => {
+          return (
+            <div className="btn" value={letter} onClick={() => this.clicked({letter})}>
+              <span>{letter}</span>
+            </div>
+          )
+        })}
+        </div>
+        <div class="row">
+        {this.state.board3.map((letter, index) => {
+          return (
+            <div className="btn" value={letter} onClick={() => this.clicked({letter})}>
+              <span>{letter}</span>
+            </div>
+          )
+        })}
+        </div>
+        <div class="row">
+        {this.state.board4.map((letter, index) => {
+          return (
+            <div className="btn" value={letter} onClick={() => this.clicked({letter})}>
+              <span>{letter}</span>
+            </div>
+          )
+        })}
+        </div>
+        <div class="row">
+        {this.state.board5.map((letter, index) => {
+          return (
+            <div className="btn" value={letter} onClick={() => this.clicked({letter})}>
+              <span>{letter}</span>
+            </div>
+          )
+        })}
+        </div>
+      </div> &nbsp;
 
        <div id="cw-submit">
         <button type="button" id="submit"> Submit Word </button>
@@ -85,7 +150,6 @@ render () {
     </div>
   )
 }
-
 }
 
 export default App;
